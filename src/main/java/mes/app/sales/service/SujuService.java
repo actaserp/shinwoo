4c2d217ffa40b018a906f196aa982ffdfe7bedc7
DESKTOP-TG6HNLE\Act-PC-11_yuri <kyl1563@naver.com>
2025-09-02 10:34:47 +0900

    2025.09.02 - 김유리 수주 등록 수정

--- src/main/java/mes/app/sales/service/SujuService.java
+++ src/main/java/mes/app/sales/service/SujuService.java
@@ -292,7 +292,7 @@ public class SujuService {
 				     ON sc_ship."Code" = s.final_state AND sc_ship."CodeType" = 'shipment_state'
 				 LEFT JOIN sys_code sc_suju
 				     ON sc_suju."Code" = s.final_state AND sc_suju."CodeType" = 'suju_state'
-				 ORDER BY s."JumunDate",s."SujuQty2" ;
+				 ORDER BY s."JumunDate",s.suju_id;
 				 
 		""";
 
